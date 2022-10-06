510751e59172ce0bb8ae6c8b70a4cf580a960588
kRa554C064 <fkonnertz@posteo.de>
2022-10-07 00:41:41 +0200

    update RebaseCtrl

--- BaseCtrl.py
+++ BaseCtrl.py
@@ -107,20 +107,7 @@ class BaseCtrl:
         else:
           self.context['username'] = "Not logged in"
           self.context['logged_in'] = False
-          self.context['username'] = "Nobody !"
 
-    """
-    def init_logging(self):
-        self.lg = logging.getLogger('test')
-        if not getattr(self.lg, 'handler_set', None):
-            fh = logging.handlers.TimedRotatingFileHandler(TMPPATH+'/log/debug.log', when='midnight')
-            fmt = '%(module)s,%(lineno)d - %(levelname)s - %(message)s'
-            form = logging.Formatter(fmt=fmt)
-            fh.setFormatter(form)
-            self.lg.addHandler(fh)
-            self.lg.setLevel(logging.DEBUG)
-        self.handler_set = True
-    """
 
     def access_denied(self):
         # self.context needed
